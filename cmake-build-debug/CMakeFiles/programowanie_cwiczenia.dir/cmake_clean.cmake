2a261269bb96d4dbb51d1978593123a60f169b03
Mateusz Szewczyk <mateusz.szewczyk000@gmail.com>
2024-03-16 15:29:37 +0100

    .1

---
+++ cmake-build-debug/CMakeFiles/programowanie_cwiczenia.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/programowanie_cwiczenia.dir/bubble_sort.c.o"
+  "CMakeFiles/programowanie_cwiczenia.dir/bubble_sort.c.o.d"
+  "programowanie_cwiczenia"
+  "programowanie_cwiczenia.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/programowanie_cwiczenia.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/programowanie_cwiczenia.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for programowanie_cwiczenia.
+# This may be replaced when dependencies are built.
